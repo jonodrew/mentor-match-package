a8b9a97ccb7a2848d980540724366bd001862c60
jonathankerr <jonathan.kerr@education.gov.uk>
2021-11-21 16:15:26 +0000

    Add __main__.py file so the package can be used
    This is a minor change

---
+++ matching/__main__.py
@@ -0,0 +1,25 @@
+import argparse
+import logging
+from pathlib import Path
+
+from matching.process import conduct_matching, create_mailing_list
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Match mentors to mentees. Your files should be called "mentees.csv" and "mentors.csv".'
+    )
+    parser.add_argument(
+        "filepath", type=str, help="the path to the data containing the files"
+    )
+    path_to_data = Path(parser.parse_args().filepath)
+    logging.info("Beginning matching exercise. This might take up to five minutes.")
+    mentors, mentees = conduct_matching(path_to_data)
+    logging.info("Matches found. Exporting to output folder!")
+    out_put_folder = path_to_data / "output"
+    create_mailing_list(mentors, out_put_folder)
+    create_mailing_list(mentees, out_put_folder)
+
+
+if __name__ == "__main__":
+    main()
