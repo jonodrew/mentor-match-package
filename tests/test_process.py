872860f922bf8b601b7c48c4a8fe15706f1936d8
jonathan <jonodrew@users.noreply.github.com>
2022-01-15 10:47:09 +0000

    Remove test that never runs
    Having a test that relies on personal data is definitely not a good idea. I might bring this back later, but for now I'm content the software is sufficiently tested

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,13 +1,7 @@
 import csv
 import logging
-import os
-import pathlib
-from typing import List
-
-import pytest
 
 from matching.mentor import Mentor
-from matching.person import Person
 from matching.process import (
     create_participant_list_from_path,
     Mentee,
@@ -54,37 +48,6 @@ class TestProcess:
         )
         logging.debug(
             f"Mentees without a mentor: {every_mentee_has_a_mentor.count(False)}"
-        )
-        assert all(every_mentee_has_a_mentor)
-
-    @pytest.mark.skipif(
-        os.environ.get("TEST") is None, reason="can't put integration data on Github"
-    )
-    def test_integration_data(self):
-        def _unmatchables(list_participants: List[Person]):
-            return len(
-                [
-                    participant
-                    for participant in list_participants
-                    if participant.has_no_match and len(participant.connections) == 0
-                ]
-            )
-
-        mentors, mentees = conduct_matching_from_file(
-            pathlib.Path(".").absolute() / "integration"
-        )
-        every_mentee_has_a_mentor = list(
-            map(lambda mentee: len(mentee.mentors) > 0, mentees)
-        )
-        logging.info(
-            f"Mentees without a mentor: {every_mentee_has_a_mentor.count(False)}\n"
-            f"Mentors without any mentees {list(map(lambda mentor: len(mentor.mentees) > 0, mentors)).count(False)}"
-        )
-        logging.info(
-            f"Total matches made: {sum(map(lambda participant: len(participant.connections), mentees))}"
-        )
-        logging.info(
-            f"Unmatchable mentors: {_unmatchables(mentors)} | mentees: {_unmatchables(mentees)}"
         )
         assert all(every_mentee_has_a_mentor)
 
