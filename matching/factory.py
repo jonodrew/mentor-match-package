709f4279b4e578533e4c60ffa01f60288890ae5b
jonathan <jonodrew@users.noreply.github.com>
2022-06-25 17:54:42 +0100

    Add type hint for Factory return

--- matching/factory.py
+++ matching/factory.py
@@ -1,15 +1,17 @@
-from typing import Dict, List
+from typing import Dict, List, TypeVar, Union
 
 from matching.mentee import Mentee
 from matching.mentor import Mentor
 from matching.person import Person
+
+Participant = TypeVar("Participant", bound=Union[Mentor, Mentee])
 
 
 class ParticipantFactory:
     participant_types = {"mentor": Mentor, "mentee": Mentee}
 
     @classmethod
-    def create_from_dict(cls, data_as_dict: Dict):
+    def create_from_dict(cls, data_as_dict: Dict) -> Participant:
         participant_type_str = list(data_as_dict).pop()
         participant_type = cls.participant_types.get(participant_type_str, Person)
         participant_data = data_as_dict.get(participant_type_str, dict())
